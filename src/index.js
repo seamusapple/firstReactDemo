5bbb198eb3c683f050768e27e60671450fdcca9c
Ramsey <seamusapple@163.com>
2022-02-25 14:58:54 +0800

    [Issue]: Add some configurations. [Root Cause]: NA [Solution]: NA [Card Number]: NA [Note]: NA

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+// var element = React.createElement(
+//   "h1",
+//   { className: "greeting" },
+//   "Hello, world!"
+// );
+// ReactDOM.render(element, document.getElementById("root"));
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
